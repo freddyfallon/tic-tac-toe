76c098e472b65404d38abc344450e29b99e9ea7e
Freddy Fallon <freddyfallon@gmail.com>
2017-05-18 14:44:21 +0100

    Add ability to change turns

--- lib/game.rb
+++ lib/game.rb
@@ -1,18 +1,24 @@
 require_relative 'board'
 require_relative 'player'
 class Game
-  attr_reader :player_1, :player_2, :board
+  attr_reader :player_1, :player_2, :board, :current_turn
   def initialize(args)
     @player_1 = args[:player_1]
     @player_2 = args[:player_2]
     @board = args[:board]
+    @current_turn = @player_1
   end
 
   def play(value)
-    self.board[value] = value
+    self.board.grid[value] = current_turn.mark
+    change_turn
+  end
+
+  def change_turn
+    current_turn == player_1 ? self.current_turn = player_2 : self.current_turn = player_1
   end
 
   private
-  attr_writer :board
+  attr_writer :board, :current_turn
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -22,6 +22,40 @@ describe Game do
         expect(game.board).to eq board
       end
     end
+
+    describe '#play' do
+      context 'Players and boards have grid and marks' do
+        before do
+          allow(game.board).to receive(:grid).and_return(
+          ["", "", "", "", "", "", "", "", ""])
+          allow(game.player_1).to receive(:mark).and_return('X')
+          allow(game.player_2).to receive(:mark).and_return('O')
+        end
+        it 'should change board value to X for player_1' do
+          game.play(0)
+          expect(game.board.grid[0]).to eq 'X'
+        end
+
+        it 'should change board value to X for player_2' do
+          game.play(0)
+          game.play(1)
+          expect(game.board.grid[1]).to eq 'O'
+        end
+      end
+    end
+  end
+
+  describe '#change_turns' do
+    it 'should change from player_1 to player_2' do
+      game.change_turn
+      expect(game.current_turn).to eq(player_2)
+    end
+
+    it 'should change from player_2 to player_1' do
+      game.change_turn
+      game.change_turn
+      expect(game.current_turn).to eq(player_1)
+    end
   end
 
 end
